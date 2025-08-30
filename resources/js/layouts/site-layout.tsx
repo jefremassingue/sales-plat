c0c944354ed1fff602417fccae2f01bb0c98c6ef
Jefre Massingue <jefremasa@gmail.com>
2025-08-30 11:09:22 +0200

    feat: refactor Google Analytics integration to use gtag.js script directly

--- resources/js/layouts/site-layout.tsx
+++ resources/js/layouts/site-layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AnalyticsProvider } from '@keiko-app/react-google-analytics';
+import { useEffect } from 'react';
 import Header from '@/layouts/site/Header';
 import Footer from '@/layouts/site/Footer';
 import { CartProvider } from '@/contexts/CartContext';
@@ -10,28 +10,41 @@ import { Toaster } from '@/components/ui/toaster';
 
 interface SiteLayoutProps { children: React.ReactNode }
 export default function SiteLayout({ children }: SiteLayoutProps) {
-    const analyticsConfig = {
-        measurementId: import.meta.env.VITE_APP_GA4_MEASUREMENT_ID,
-        // debug: true, // Enable for development only
-        // verbose: true, // Enable for development only
-    };
+    useEffect(() => {
+        // Inject Google Analytics gtag.js script
+        const scriptTag = document.createElement('script');
+        scriptTag.async = true;
+        scriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=G-1K545DKM2W';
+        document.head.appendChild(scriptTag);
 
-    // console.log(analyticsConfig);
+        // Inject gtag config
+        const inlineScript = document.createElement('script');
+        inlineScript.innerHTML = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1K545DKM2W');
+        `;
+        document.head.appendChild(inlineScript);
+
+        return () => {
+            document.head.removeChild(scriptTag);
+            document.head.removeChild(inlineScript);
+        };
+    }, []);
 
     return (
-        <AnalyticsProvider config={analyticsConfig}>
-            <CartProvider>
-                <div className="flex min-h-screen flex-col bg-white text-zinc-700">
-                    <Header />
-                    <main className="flex-1">
-                        {children}
-                    </main>
-                    <Footer />
-                    <Toaster />
-                    <ShoppingCart />
-                </div>
-            </CartProvider>
-        </AnalyticsProvider>
+        <CartProvider>
+            <div className="flex min-h-screen flex-col bg-white text-zinc-700">
+                <Header />
+                <main className="flex-1">
+                    {children}
+                </main>
+                <Footer />
+                <Toaster />
+                <ShoppingCart />
+            </div>
+        </CartProvider>
     );
 }
 
