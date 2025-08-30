fb893ac06f188f9e75cec75a84bab1d53630ce79
Jefre Massingue <jefremasa@gmail.com>
2025-08-30 10:52:50 +0200

    feat: integrate Google Analytics with configuration in site layout

--- resources/js/layouts/site-layout.tsx
+++ resources/js/layouts/site-layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AnalyticsProvider } from '@keiko-app/react-google-analytics';
 import Header from '@/layouts/site/Header';
 import Footer from '@/layouts/site/Footer';
 import { CartProvider } from '@/contexts/CartContext';
@@ -9,18 +10,28 @@ import { Toaster } from '@/components/ui/toaster';
 
 interface SiteLayoutProps { children: React.ReactNode }
 export default function SiteLayout({ children }: SiteLayoutProps) {
+    const analyticsConfig = {
+        measurementId: import.meta.env.VITE_APP_GA4_MEASUREMENT_ID,
+        // debug: true, // Enable for development only
+        // verbose: true, // Enable for development only
+    };
+
+    // console.log(analyticsConfig);
+
     return (
-        <CartProvider>
-            <div className="flex min-h-screen flex-col bg-white text-zinc-700">
-                <Header />
-                <main className="flex-1">
-                    {children}
-                </main>
-                <Footer />
-                <Toaster />
-                <ShoppingCart />
-            </div>
-        </CartProvider>
+        <AnalyticsProvider config={analyticsConfig}>
+            <CartProvider>
+                <div className="flex min-h-screen flex-col bg-white text-zinc-700">
+                    <Header />
+                    <main className="flex-1">
+                        {children}
+                    </main>
+                    <Footer />
+                    <Toaster />
+                    <ShoppingCart />
+                </div>
+            </CartProvider>
+        </AnalyticsProvider>
     );
 }
 
